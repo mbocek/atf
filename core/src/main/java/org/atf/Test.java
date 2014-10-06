35c8a2292422d04bd585896a77bdc3cc3b5376e9
Michal Bocek <michal.bocek@gmail.com>
2014-10-06 08:00:39 +0000

    first module - core

--- core/src/main/java/org/atf/Test.java
+++ core/src/main/java/org/atf/Test.java
@@ -18,7 +18,13 @@
  */
 package org.atf;
 
-@Docummeted
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
+
+@Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
